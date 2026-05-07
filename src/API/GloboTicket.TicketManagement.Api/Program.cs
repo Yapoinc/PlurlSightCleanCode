a1fd2d0f217509289f4f2954926ebbcd72148811
Bernardo <yapoinc@gmail.com>
2026-05-07 15:48:09 -0300

    Fistmigration

--- src/API/GloboTicket.TicketManagement.Api/Program.cs
+++ src/API/GloboTicket.TicketManagement.Api/Program.cs
@@ -1,20 +1,9 @@
 using GloboTicket.TicketManagement.Api;
-using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder
 .ConfigureServices()
 .ConfigurePipeline();
-
-
-
-// Configure the HTTP request pipeline.
-
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
-
-// app.MapControllers();
+await app.ResetDatabaseAsync();
 
 app.Run();
--- src/API/GloboTicket.TicketManagement.Api/StartupExtensions.cs
+++ src/API/GloboTicket.TicketManagement.Api/StartupExtensions.cs
@@ -1,6 +1,7 @@
 using GloboTicket.TicketManagement.Application;
 using GloboTicket.TicketManagement.Infrastructure;
 using GloboTicket.TicketManagement.Persistence;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 // using GloboTicket.TicketManagement.Identity;
 // using GloboTicket.TicketManagement.Identity.Models;
@@ -47,6 +48,26 @@ public static class StartupExtensions
         app.UseAuthorization();
         app.MapControllers();
         return app;
+    }
+
+    public static async Task ResetDatabaseAsync(this WebApplication app)
+    {
+
+        using var scope = app.Services.CreateScope();
+        try
+        {
+            var context = scope.ServiceProvider.GetService<GloboTicketDbContext>();
+            if (context != null)
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.MigrateAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+           // add loggin here
+        } 
+        
     }
 
 }
